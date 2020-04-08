fd3194c42dc2daa4e4492118e1386957dbe4acba
Albdroid <60861585+SxtBox@users.noreply.github.com>
2020-04-08 21:37:03 +0200

    Update Youtube_Test_HLS.php

--- Youtube_Test_HLS.php
+++ Youtube_Test_HLS.php
@@ -8,10 +8,10 @@ $html = file_get_contents("https://www.youtube.com/watch?v=$id");
 $hlsManifestUrl = '/,\\\\"hlsManifestUrl\\\\":\\\\"(.*?)\\\\"/m';
 preg_match_all($hlsManifestUrl, $html, $match, PREG_PATTERN_ORDER, 0);
     $stream = $match[1][0];
-	$stream=str_replace("\/", "/", $stream);
+    $stream=str_replace("\/", "/", $stream);
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 //header("Location: " .$stream, 302); // PER XT PANEL HAP KETTE LINE EDHE MBYLL 2 TE TJERAT QE JANE ME POSHTE
-// echo $stream;      // << PRINT RAW CODE
+// echo $stream;     // << PRINT RAW CODE
 readfile ($stream); // << READ HLS SOURCE DIRECT FROM BROWSER
 ?> 
